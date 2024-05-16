85dd0052b738f78f068ec10805635503076074a1
J. Aaron Hogan <jamesaaronhogan@gmail.com>
2024-05-16 13:27:18 -0600

    update create scripts
    now with other steps added into the script

--- create_Growth.R
+++ create_Growth.R
@@ -74,7 +74,11 @@ for (j in 1:length(state_vec)) {
   state$TREE$HT_m  <- state$TREE$HT / 3.28084
   state$TREE$ACTUALHT_m  <- state$TREE$ACTUALHT / 3.28084
   
-   state$PLOT$ELEV_m <- state$PLOT$ELEV / 3.28084
+  state$PLOT$ELEV_m <- state$PLOT$ELEV / 3.28084
+  
+  state$TREE_GRM_COMPONENT$DIA_BEGIN_cm <- state$TREE_GRM_COMPONENT$DIA_BEGIN * 2.54
+  state$TREE_GRM_COMPONENT$DIA_END_cm <- state$TREE_GRM_COMPONENT$DIA_END * 2.54
+  
   
   ##  create pltID - unique plot identifier string
   state$TREE$pltID <-  stringr::str_c(state$TREE$UNITCD, state$TREE$STATECD, state$TREE$COUNTYCD, state$TREE$PLOT, sep = '_')
@@ -95,7 +99,7 @@ for (j in 1:length(state_vec)) {
   
   ## make placeholder data fame - each row with a single tree record 
   ## selects 
-  calc_g_df <-  tree_df  %>% select(PLT_CN, pltID, PLOT, SUBP, CN, PREV_TRE_CN, INVYR, CYCLE, TREE, treeID, STATUSCD, SPCD, SPGRPCD, PREVDIA_cm, DIA_cm, DIAHTCD, HT_m, ACTUALHT_m, TPHa_UNADJ, CR, CCLCD, CLIGHTCD, DIACHECK, DAMLOC1, DAMTYP1, DAMSEV1) %>% arrange(treeID, INVYR)
+  calc_g_df <-  tree_df  %>% select(PLT_CN, pltID, PLOT, SUBP, CN, PREV_TRE_CN, INVYR, CYCLE, TREE, treeID, STATUSCD, SPCD, SPGRPCD, PREVDIA_cm, DIA_cm, DIAHTCD, HT_m, ACTUALHT_m, TPHa_UNADJ, CR, CCLCD, CLIGHTCD, DIACHECK, RECONCILECD, DAMLOC1, DAMTYP1, DAMSEV1) %>% arrange(treeID, INVYR)
   
   ## change CN colname to TREE_CN
   colnames(calc_g_df)[5] <- "TRE_CN"
@@ -104,7 +108,7 @@ for (j in 1:length(state_vec)) {
   calc_g_df$STATE <- state$SURVEY$STATEAB[1]
   
   ## merge in the tree_grm (TREE_GRM_COMPONENT) table 
-  calc_g_df <-  dplyr::left_join(calc_g_df, tree_grm[,c("TRE_CN", "PREV_TRE_CN", "PLT_CN", "STEM_COMPONENT")],
+  calc_g_df <-  dplyr::left_join(calc_g_df, tree_grm[,c("TRE_CN", "PREV_TRE_CN", "PLT_CN", "DIA_BEGIN_cm", "DIA_END_cm", "STEM_COMPONENT")],
                                  by = c("TRE_CN", "PREV_TRE_CN", "PLT_CN"))
   
   ## merge in the plot table -- need this for REMPER and MEASTIMES etc. 
@@ -161,8 +165,13 @@ for (j in 1:length(state_vec)) {
 
   ######################################################################
     
-  calc_g_df$AGR_DIA_mm <- ((calc_g_df$DIA_cm - calc_g_df$PREVDIA_cm)*10) / calc_g_df$REMPER
-  calc_g_df$RGR_DIA_mm <- ((log(calc_g_df$DIA_cm) - log(calc_g_df$PREVDIA_cm))*10) / calc_g_df$REMPER
+  calc_g_df$AGR_DIA_mm <- ifelse(!is.na(calc_g_df$PREVDIA_cm), 
+                                 ((calc_g_df$DIA_cm - calc_g_df$PREVDIA_cm)*10) / calc_g_df$REMPER, 
+                                 ((calc_g_df$DIA_cm - calc_g_df$DIA_BEGIN_cm)*10) / calc_g_df$REMPER)
+  
+  calc_g_df$RGR_DIA_mm <- ifelse(!is.na(calc_g_df$PREVDIA_cm), 
+                                 ((log(calc_g_df$DIA_cm) - log(calc_g_df$PREVDIA_cm))*10) / calc_g_df$REMPER, 
+                                 ((log(calc_g_df$DIA_cm) - log(calc_g_df$DIA_BEGIN_cm))*10) / calc_g_df$REMPER)
   
   #####################################################################################################
   #####################################################################################################
@@ -221,7 +230,166 @@ for (j in 1:length(state_vec)) {
 
 Caribbean_G <- rbind(PR_calc_g_df, VI_calc_g_df)
 
+
+################################################################################################################################
+################################################################################################################################
+## note: to add climate and climate change variables (calculated at the subplot scale) to this dataset run lines 324-394 of Caribbean_FIA_Climate_plotting_and_mapping.Rmd
+
+## read in the datasets
+# ##### climate normals -- historic -- PRISM data 
+clim_baseline <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/PRISM/clim_baseline.Rds")
+
+### adjust ppt to monthly
+### divide by 12 to get annual average to monthly average
+clim_baseline$X30_yr_precip_normal_MONTHLY <- clim_baseline$X30_yr_precip_normal / 12
+
+## timeseries of climate data -- TERRACLIM data
+ppt_subplots <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/terraClimate/ppt_subplots_TerraClim_extraction.RDS")
+tmax_subplots <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/terraClimate/tmax_subplots_TerraClim_extraction.RDS")
+
+## read in the saved RDS data objects -- see "SUBP_temp_and_precip_change.Rmd"
+
+tmax_subplots$subpID <-  as.factor(tmax_subplots$subpID)
+ppt_subplots$subpID <-  as.factor(ppt_subplots$subpID)
+
+## create a year-mon date variables in each dataset
+tmax_subplots$year_mon <-  lubridate::parse_date_time(paste(tmax_subplots$month, tmax_subplots$year, sep = "-"), '%m-%Y')
+
+ppt_subplots$year_mon <- lubridate::parse_date_time(paste(ppt_subplots$month, ppt_subplots$year, sep = "-"), '%m-%Y')
+
+
+
+
+### need to create subpID for comparison to survival and growth datasets
+Caribbean_G$subpID <- gsub('.{2}$', '', Caribbean_G$treeID)
+
+### some lingering subpIDs have an extra "_" -- need to remove
+Caribbean_G$subpID <- ifelse(stringr::str_sub(Caribbean_G$subpID,  -1) == "_", substr(Caribbean_G$subpID, 1, nchar(Caribbean_G$subpID) - 1), Caribbean_G$subpID)
+
+
+### create the dummy variables with -9999 values -- to be overwritten by for loop
+Caribbean_G$X_30yr_precip_normal_MONTHLY <- -9999
+Caribbean_G$X_30yr_temp_normal           <- -9999
+
+Caribbean_G$Avg_MaxTemp_CenInterval      <- -9999
+Caribbean_G$Avg_ppt_CenInterval          <- -9999
+
+Caribbean_G$diff_MaxTemp                 <- -9999
+Caribbean_G$diff_ppt                     <- -9999
+
+
+ymd_function  <- function(x) {year <- floor(x);
+                              month <- floor((x - year) * 12);
+                              day <- ((x - year) * 12 - month) * 30.42;
+                              return(list(year = year, month = month, day = day))
+                              }
+
+## use a for loop to calculate climate difference
+
+for (j in 1:dim(Caribbean_G)[1]) {
+
+  ###  get the pltID and other important data from the Caribbean_G data frame
+  plt <- Caribbean_G$subpID[j]
+  mon <- ifelse(is.na(Caribbean_G[j,"MEASMON"]), 6, Caribbean_G[j,"MEASMON"])
+  yr <-  ifelse(is.na(Caribbean_G[j,"MEASYEAR"]), Caribbean_G[j,"INVYR"], Caribbean_G[j,"MEASYEAR"])
+  REMPER <- ifelse(is.na(Caribbean_G[j,"REMPER"]), 5.0, Caribbean_G[j,"REMPER"])
+
+
+  ### pull the precip and max temp normal data from the PRISM calculations and add to the dataset
+  ## take mean (na.rm = T), in cases where you might have double extractions from the baseline -- not sure why... but they exist.
+  Caribbean_G$X_30yr_precip_normal_MONTHLY[j] <-  mean(clim_baseline[clim_baseline$subpID == Caribbean_G$subpID[j],"X30_yr_precip_normal_MONTHLY"], na.rm = T)
+  Caribbean_G$X_30yr_temp_normal[j] <- mean(clim_baseline[clim_baseline$subpID == Caribbean_G$subpID[j],"X30_yr_temp_normal"], na.rm = T)
+
+  #### set up some things to help with the sub-setting
+  #### END Date year_mon  is the measurement time at T2
+  ENDMEAS_date <-  lubridate::parse_date_time(paste(mon, yr , sep = "-"), '%m-%Y')
+
+  ## dealing with decimal REMPERS
+  x <- ymd_function(REMPER)
+
+  #### START Date year_mon is the END Date year_mon minus REMPER
+  STARTMEAS_date <-  ENDMEAS_date - lubridate::period(c(x$year, x$month, round(x$day)), c("year", "months", "days"))
+
+  RANGE_year_mon <- seq(from = lubridate::floor_date(STARTMEAS_date , unit = "months"), to = ENDMEAS_date, by = "month")  ## range
+
+  ### subsetting
+  tmax_data <- tmax_subplots[tmax_subplots$subpID == plt & tmax_subplots$year_mon %in% RANGE_year_mon, "tmax"]
+
+  ppt_data <- ppt_subplots[ppt_subplots$subpID == plt & ppt_subplots$year_mon %in% RANGE_year_mon, "ppt"]
+
+  ### get the average of the values over the census interval
+  Caribbean_G$Avg_MaxTemp_CenInterval[j] <- mean(tmax_data, na.rm = T)
+
+  Caribbean_G$Avg_ppt_CenInterval[j] <- mean(ppt_data, na.rm = T)
+
+  ### calculate difference between census interval and climate norma
+  Caribbean_G$diff_MaxTemp[j] <- Caribbean_G$Avg_MaxTemp_CenInterval[j] - Caribbean_G$X_30yr_temp_normal[j]
+
+  Caribbean_G$diff_ppt[j] <- Caribbean_G$Avg_ppt_CenInterval[j] -  Caribbean_G$X_30yr_precip_normal[j]
+
+}
+
+################################################################################################################################
+################################################################################################################################
+## note: to add canopy height and canopy height change variables (also calculated at the subplot scale) to this dataset run lines 1073-1175 of Li-Dar_CanopyCover_and_TreeHeight_Regressions.Rmd
+
+### load the dataset remote sensing dataset 
+library(XLConnect)
+
+#### NEW DATASET
+wb2 <- loadWorkbook("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/LiDarData_from_Tom_R/lidar_7p5m_rad_nd_pwd.xlsx", password = "PuertoRico")
+
+setMissingValue(wb2, value = -9999)  ## set the missing data value to -9999
+
+lidar_data2 <- readWorksheet(wb2, "lidar_7p5m_rad_nd")
+
+### deal with small negative values in Lidar canopy heights
+lidar_data2[!is.na(lidar_data2$CanopyHeight2016) & lidar_data2$CanopyHeight2016 < 0,]$CanopyHeight2016 <- 0
+
+lidar_data2[!is.na(lidar_data2$CanopyHeight2018) & lidar_data2$CanopyHeight2018 < 0,]$CanopyHeight2018 <- 0
+
+
+# ## compute the mean plot height and add at the data frame
+Caribbean_G <-  Caribbean_G %>% group_by(subpID, INVYR) %>% mutate(mean_subp_HT_m = mean(HT_m, na.rm = T)) %>% ungroup()
+
+# for the last / most recent inventory (2016 - 2021;  cycles 6-7) -- replace mean_subp_HT_m with the Lidar data ---
+# all plot measurements before SEPTEMBER 2017 gets the 2016/17 USGS Lidar height data
+# everything after
+
+## first do 2016 -- overwrites values with Lidar values
+Caribbean_G[Caribbean_G$INVYR == 2016,]$mean_subp_HT_m <- lidar_data2[match(Caribbean_G[Caribbean_G$INVYR == 2016,]$subpID, lidar_data2$subpID),]$CanopyHeight2016
+
+## then do 2017 -- jan - sept
+Caribbean_G[Caribbean_G$INVYR == 2017 & Caribbean_G$MEASMON %in% c(1,2,3,4,5,6,7,8),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_G[Caribbean_G$INVYR == 2017 & Caribbean_G$MEASMON %in% c(1,2,3,4,5,6,7,8),]$subpID, lidar_data2$subpID),]$CanopyHeight2016
+
+## 2017 - after sept
+Caribbean_G[Caribbean_G$INVYR == 2017 & Caribbean_G$MEASMON %in% c(9,10,11,12),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_G[Caribbean_G$INVYR == 2017 & Caribbean_G$MEASMON %in% c(9,10,11,12),]$subpID, lidar_data2$subpID),]$CanopyHeight2018
+
+## 2018, 2019, 2021
+Caribbean_G[Caribbean_G$INVYR %in% c("2018", "2019", "2021"),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_G[Caribbean_G$INVYR %in% c("2018", "2019", "2021"),]$subpID, lidar_data2$subpID),]$CanopyHeight2018
+
+# CODE FOR CANOPY HEIGHT DIFFERENCE
+## for loop-de-loop !!
+xxx <- -9999
+
+for (i in 1:length(Caribbean_G$pltID)) {
+  
+  xxx[i] <- unlist(if (Caribbean_G$INVYR[i] %in% c(2001,2002,2003,2004)) {
+    0   #if in first census interval then canopy change is zero
+  } else {
+    ifelse(nrow(Caribbean_G[Caribbean_G$treeID %in% Caribbean_G$treeID[i] & Caribbean_G$INVYR %in% (Caribbean_G$INVYR[i] - 5), "mean_subp_HT_m"]) > 0,
+           Caribbean_G[Caribbean_G$treeID %in% Caribbean_G$treeID[i] & Caribbean_G$INVYR %in% Caribbean_G$INVYR[i], "mean_subp_HT_m"] -
+             Caribbean_G[Caribbean_G$treeID %in% Caribbean_G$treeID[i] & Caribbean_G$INVYR %in% (Caribbean_G$INVYR[i] - 5), "mean_subp_HT_m"], 0)
+    # if in second interval and missing data, then canopy change is zero
+  } )
+}
+
+Caribbean_G$Delta_CanopyHeight <- xxx
+
+################################################################################################################################
+################################################################################################################################
 ## save result
 saveRDS(Caribbean_G , file = paste(save_object_path, "Caribbean_G.Rds", sep = ""))
+
 
 
--- create_Survival.R
+++ create_Survival.R
@@ -63,6 +63,9 @@ if (state_vec[j] == "PR") {
   state$TREE$TPHaGROW_UNADJ <-  state$TREE$TPAGROW_UNADJ * 2.4710538147   ##TPA base level TPA
 
   
+  state$TREE_GRM_COMPONENT$DIA_BEGIN_cm <- state$TREE_GRM_COMPONENT$DIA_BEGIN * 2.54
+  state$TREE_GRM_COMPONENT$DIA_END_cm <- state$TREE_GRM_COMPONENT$DIA_END * 2.54
+  
   ##  create pltID - unique plot identifier string
   state$TREE$pltID <- stringr::str_c(state$TREE$UNITCD, state$TREE$STATECD, state$TREE$COUNTYCD, state$TREE$PLOT, sep = '_')
   state$PLOT$pltID <- stringr::str_c(state$PLOT$UNITCD, state$PLOT$STATECD, state$PLOT$COUNTYCD, state$PLOT$PLOT, sep = '_')
@@ -93,7 +96,7 @@ if (state_vec[j] == "PR") {
   ### tree_ref_df - from TREE table
   tree_ref_df <- state$TREE %>% 
                  select(pltID, treeID, CN, PLT_CN, INVYR, CYCLE, PREV_TRE_CN, TREE, SPCD, SPGRPCD, STATUSCD, 
-                        PREVCOND, RECONCILECD, MORTCD, DIA_cm, PREVDIA_cm, DIACHECK,  
+                        PREVCOND, RECONCILECD, MORTCD, DIA_cm, PREVDIA_cm, DIACHECK,  RECONCILECD,
                         TPHa_UNADJ, TPHaGROW_UNADJ, HT_m, ACTUALHT_m, BOLEHT_m, CR, CCLCD, 
                         CPOSCD, CLIGHTCD, CDENCD, DAMLOC1, DAMTYP1, DAMSEV1) %>% 
                  arrange(pltID, INVYR, TREE)
@@ -142,7 +145,7 @@ if (state_vec[j] == "PR") {
   if ("TREE_GRM_COMPONENT" %in% ls(state)) {
     
     ### GRM_ref_df 
-    GRM_ref_df <- state$TREE_GRM_COMPONENT %>% select(TRE_CN, PREV_TRE_CN, PLT_CN, STEM_COMPONENT)
+    GRM_ref_df <- state$TREE_GRM_COMPONENT %>% select(TRE_CN, PREV_TRE_CN, DIA_BEGIN_cm, DIA_END_cm, PLT_CN, STEM_COMPONENT)
     
     ### merge tree_ref_df with GRM_ref_df
     merged_tree_df <- left_join(tree_ref_df, GRM_ref_df, by = c("TRE_CN", "PREV_TRE_CN", "PLT_CN"))  %>% 
@@ -194,15 +197,19 @@ if (state_vec[j] == "PR") {
   ds <- left_join(merged_tree_df, cnd_plt_srv, by = c("pltID", "PLT_CN", "INVYR", "CYCLE"), 
                   relationship = "many-to-one")
 
+
   ## LASTLY:  CALCULATE CROWN LENGTH ==== Height X compacted crown ratio (from T1 measurement ) / 100
   ## add 1 number to not have zeros -- log(0) is - Inf
   ds$CROWN_LENGTH <- (ds$HT_m * (ds$PREV_CR)/100) + 1  
   
   ## calculate growth of trees in tree_ref_df
-  ds$AGR_DIA_mm <- ((ds$DIA_cm - ds$PREVDIA_cm)*10) / ds$REMPER
-  ds$RGR_DIA_mm <- ((log(ds$DIA_cm) - log(ds$PREVDIA_cm))*10) / ds$REMPER
-  
-  
+  ds$AGR_DIA_mm <- ifelse(!is.na(ds$PREVDIA_cm), 
+                          ((ds$DIA_cm - ds$PREVDIA_cm)*10) / ds$REMPER, 
+                          ((ds$DIA_cm - ds$DIA_BEGIN_cm)*10) / ds$REMPER)
+  ds$RGR_DIA_mm <- ifelse(!is.na(ds$PREVDIA_cm), 
+                          ((log(ds$DIA_cm) - log(ds$PREVDIA_cm))*10) / ds$REMPER, 
+                          ((log(ds$DIA_cm) - log(ds$DIA_BEGIN_cm))*10) / ds$REMPER)
+ 
   ### add state prefix to Robject -- re-assign calc_df
   assign(paste(state_vec[j], "_ds", sep = ""), ds)
   
@@ -215,5 +222,167 @@ if (state_vec[j] == "PR") {
 
 ### combine data -- PR and USVI 
 Caribbean_ds <- rbind(PR_ds, VI_ds)
+
+
+################################################################################################################################
+################################################################################################################################
+## note: to add climate and climate change variables (calculated at the subplot scale) to this dataset run lines 243-317 of Caribbean_FIA_Climate_plotting_and_mapping.Rmd
+
+## read in the datasets
+# ##### climate normals -- historic -- PRISM data 
+clim_baseline <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/PRISM/clim_baseline.Rds")
+
+### adjust ppt to monthly
+### divide by 12 to get annual average to monthly average
+clim_baseline$X30_yr_precip_normal_MONTHLY <- clim_baseline$X30_yr_precip_normal / 12
+
+## timeseries of climate data -- TERRACLIM data
+ppt_subplots <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/terraClimate/ppt_subplots_TerraClim_extraction.RDS")
+tmax_subplots <- readRDS("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/terraClimate/tmax_subplots_TerraClim_extraction.RDS")
+
+## read in the saved RDS data objects -- see "SUBP_temp_and_precip_change.Rmd"
+
+tmax_subplots$subpID <-  as.factor(tmax_subplots$subpID)
+ppt_subplots$subpID <-  as.factor(ppt_subplots$subpID)
+
+## create a year-mon date variables in each dataset
+tmax_subplots$year_mon <-  lubridate::parse_date_time(paste(tmax_subplots$month, tmax_subplots$year, sep = "-"), '%m-%Y')
+
+ppt_subplots$year_mon <- lubridate::parse_date_time(paste(ppt_subplots$month, ppt_subplots$year, sep = "-"), '%m-%Y')
+
+### need to create subpID for comparison to survival and growth datasets
+Caribbean_ds$subpID <- gsub('.{2}$', '', Caribbean_ds$treeID)
+
+### some lingering subpIDs have an extra "_" -- need to remove
+Caribbean_ds$subpID <- ifelse(stringr::str_sub(Caribbean_ds$subpID,  -1) == "_", substr(Caribbean_ds$subpID, 1, nchar(Caribbean_ds$subpID) - 1), Caribbean_ds$subpID)
+
+
+### create the dummy variables with -9999 values -- to be overwritten by for loop
+Caribbean_ds$X_30yr_precip_normal_MONTHLY <- -9999
+Caribbean_ds$X_30yr_temp_normal           <- -9999
+
+Caribbean_ds$Avg_MaxTemp_CenInterval      <- -9999
+Caribbean_ds$Avg_ppt_CenInterval          <- -9999
+
+Caribbean_ds$diff_MaxTemp                 <- -9999
+Caribbean_ds$diff_ppt                     <- -9999
+
+
+ymd_function  <- function(x) {year <- floor(x);
+                              month <- floor((x - year) * 12);
+                              day <- ((x - year) * 12 - month) * 30.42;
+                              return(list(year = year, month = month, day = day))
+                              }
+
+## use a for loop to calculate climate difference
+
+for (j in 1:dim(Caribbean_ds)[1]) {
+
+  ###  get the pltID and other important data from the Caribbean_ds data frame
+  plt <- Caribbean_ds$subpID[j]
+  mon <- ifelse(is.na(Caribbean_ds[j,"MEASMON"]), 6, Caribbean_ds[j,"MEASMON"])
+  yr <-  ifelse(is.na(Caribbean_ds[j,"MEASYEAR"]), Caribbean_ds[j,"INVYR"], Caribbean_ds[j,"MEASYEAR"])
+  REMPER <- ifelse(is.na(Caribbean_ds[j,"REMPER"]), 5.0, Caribbean_ds[j,"REMPER"])
+
+
+  ### pull the precip and max temp normal data from the PRISM calculations and add to the dataset
+  ## take mean (na.rm = T), in cases where you might have double extractions from the baseline -- not sure why... but they exist.
+  Caribbean_ds$X_30yr_precip_normal_MONTHLY[j] <-  mean(clim_baseline[clim_baseline$subpID == Caribbean_ds$subpID[j],"X30_yr_precip_normal_MONTHLY"], na.rm = T)
+  Caribbean_ds$X_30yr_temp_normal[j] <- mean(clim_baseline[clim_baseline$subpID == Caribbean_ds$subpID[j],"X30_yr_temp_normal"], na.rm = T)
+
+  #### set up some things to help with the sub-setting
+  #### END Date year_mon  is the measurement time at T2
+  ENDMEAS_date <-  lubridate::parse_date_time(paste(mon, yr , sep = "-"), '%m-%Y')
+
+  ## dealing with decimal REMPERS
+  x <- ymd_function(REMPER)
+
+  #### START Date year_mon is the END Date year_mon minus REMPER
+  STARTMEAS_date <-  ENDMEAS_date - lubridate::period(c(x$year, x$month, round(x$day)), c("year", "months", "days"))
+
+  RANGE_year_mon <- seq(from = lubridate::floor_date(STARTMEAS_date , unit = "months"), to = ENDMEAS_date, by = "month")  ## range
+
+  ### subsetting
+  tmax_data <- tmax_subplots[tmax_subplots$subpID == plt & tmax_subplots$year_mon %in% RANGE_year_mon, "tmax"]
+
+  ppt_data <- ppt_subplots[ppt_subplots$subpID == plt & ppt_subplots$year_mon %in% RANGE_year_mon, "ppt"]
+
+  ### get the average of the values over the census interval
+  Caribbean_ds$Avg_MaxTemp_CenInterval[j] <- mean(tmax_data$tmax, na.rm = T)
+
+  Caribbean_ds$Avg_ppt_CenInterval[j] <- mean(ppt_data$ppt, na.rm = T)
+
+  ### calculate difference between census interval and climate norma
+  Caribbean_ds$diff_MaxTemp[j] <- Caribbean_ds$Avg_MaxTemp_CenInterval[j] - Caribbean_ds$X_30yr_temp_normal[j]
+
+   Caribbean_ds$diff_ppt[j] <- Caribbean_ds$Avg_ppt_CenInterval[j] -  Caribbean_ds$X_30yr_precip_normal[j]
+
+}
+
+
+################################################################################################################################
+################################################################################################################################
+## note: to add canopy height and canopy height change variables (also calculated at the subplot scale) to this dataset run lines 1073-1175 of Li-Dar_CanopyCover_and_TreeHeight_Regressions.Rmd
+
+### load the dataset remote sensing dataset 
+library(XLConnect)
+
+#### NEW DATASET
+wb2 <- loadWorkbook("C:/Users/JamesHogan/Box/James.Hogan_Workspace/ORISE_PostDoc/data/LiDarData_from_Tom_R/lidar_7p5m_rad_nd_pwd.xlsx", password = "PuertoRico")
+
+setMissingValue(wb2, value = -9999)  ## set the missing data value to -9999
+
+lidar_data2 <- readWorksheet(wb2, "lidar_7p5m_rad_nd")
+
+### deal with small negative values in Lidar canopy heights
+lidar_data2[!is.na(lidar_data2$CanopyHeight2016) & lidar_data2$CanopyHeight2016 < 0,]$CanopyHeight2016 <- 0
+
+lidar_data2[!is.na(lidar_data2$CanopyHeight2018) & lidar_data2$CanopyHeight2018 < 0,]$CanopyHeight2018 <- 0
+
+
+## compute the mean plot height and add at the data frame
+Caribbean_ds <-  Caribbean_ds %>% group_by(subpID, INVYR) %>% mutate(mean_subp_HT_m = mean(HT_m, na.rm = T)) %>% ungroup()
+
+
+## for the last / most recent inventory (2016 - 2021;  cycles 6-7) -- replace mean_subp_HT_m with the Lidar data ---
+## all plot measurements before SEPTEMBER 2017 gets the 2016/17 USGS Lidar height data
+## everything after
+
+######## SURVIVAL DATASET
+## first do 2016 -- overwrites values with Lidar values
+Caribbean_ds[Caribbean_ds$INVYR == 2016,]$mean_subp_HT_m <- lidar_data2[match(Caribbean_ds[Caribbean_ds$INVYR == 2016,]$subpID, lidar_data2$subpID),]$CanopyHeight2016
+
+## then do 2017 -- jan - sept
+Caribbean_ds[Caribbean_ds$INVYR == 2017 & Caribbean_ds$MEASMON %in% c(1,2,3,4,5,6,7,8),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_ds[Caribbean_ds$INVYR == 2017 & Caribbean_ds$MEASMON %in% c(1,2,3,4,5,6,7,8),]$subpID, lidar_data2$subpID),]$CanopyHeight2016
+
+## 2017 - after sept
+Caribbean_ds[Caribbean_ds$INVYR == 2017 & Caribbean_ds$MEASMON %in% c(9,10,11,12),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_ds[Caribbean_ds$INVYR == 2017 & Caribbean_ds$MEASMON %in% c(9,10,11,12),]$subpID, lidar_data2$subpID),]$CanopyHeight2018
+
+## 2018, 2019, 2021
+Caribbean_ds[Caribbean_ds$INVYR %in% c("2018", "2019", "2021"),]$mean_subp_HT_m <- lidar_data2[match(Caribbean_ds[Caribbean_ds$INVYR %in% c("2018", "2019", "2021"),]$subpID, lidar_data2$subpID),]$CanopyHeight2018
+
+
+# CODE FOR CANOPY HEIGHT DIFFERENCE
+
+## for loop-de-loop !!
+
+Caribbean_ds$Delta_CanopyHeight <- -9999
+
+###
+for (i in 1:length(Caribbean_ds$pltID)) {
+
+  Caribbean_ds[i,]$Delta_CanopyHeight <- unlist(if (Caribbean_ds$INVYR[i] %in% c(2001,2002,2003,2004)) {
+                                               0   #if in first census interval then canopy change is zero
+                                        } else {
+                                            ifelse(nrow(Caribbean_ds[Caribbean_ds$treeID %in% Caribbean_ds$treeID[i] & Caribbean_ds$INVYR %in% (Caribbean_ds$INVYR[i] - 5), "mean_subp_HT_m"]) > 0,
+                                                        Caribbean_ds[Caribbean_ds$treeID %in% Caribbean_ds$treeID[i] & Caribbean_ds$INVYR %in% Caribbean_ds$INVYR[i], "mean_subp_HT_m"] -
+                                                        Caribbean_ds[Caribbean_ds$treeID %in% Caribbean_ds$treeID[i] & Caribbean_ds$INVYR %in% (Caribbean_ds$INVYR[i] - 5), "mean_subp_HT_m"], 0)
+                                                        # if in second interval and missing data, then canopy change is zero
+      } )
+}
+
+
+################################################################################################################################
+################################################################################################################################
 ## save result
 saveRDS(Caribbean_ds, file = paste(save_object_path, "Caribbean_ds.Rds", sep = ""))
